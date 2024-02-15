24a3a9797cd57f98ddc399beff655b8254dc3c36
Annelie Svensson <annelievs@hotmail.com>
2024-02-15 14:07:35 +0100

    lagt till Animering-sida, mindre justeringar i layout på övriga sidor

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,12 @@
 const toggleBtn = document.querySelector('.toggle-btn');
 const toggleBtnIcon = document.querySelector('.toggle-btn i');
 const dropDownMenu = document.querySelector('.dropdown-menu');
+const spiderBtn = document.getElementById('button');
+const spider = document.getElementById('spider');
 
 // Händelsehanterare
 toggleBtn.addEventListener("click", toggleMenu, false);
+spiderBtn.addEventListener("click", surpriseSpider, false);
 
 // Funktion för att öppna och stänga menyn i mobilläge
 function toggleMenu() {
@@ -14,4 +17,9 @@ function toggleMenu() {
     const isOpen = dropDownMenu.classList.contains('open');
 
     toggleBtnIcon.className = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+}
+
+//Funktion för spindeln
+function surpriseSpider() {
+    spider.classList.toggle('hidden');
 }
